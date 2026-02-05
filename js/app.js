ae2c97fb54c75c737a34e9b193d9b3a3c273b880
blitz-flex <abashidzeikaa@gmail.com>
2026-02-05 21:24:18 +0400

    Drag the sharing icons to the right

--- js/app.js
+++ js/app.js
@@ -358,26 +358,28 @@ async function showRecipe(id) {
 	                        🎥 Watch Video Tutorial
 	                    </a>
 	                ` : ''}
-	                <div class="share-actions" aria-label="Share recipe">
-	                    <button type="button" class="share-icon-btn" data-action="share-facebook" aria-label="Share on Facebook" title="Facebook">
-	                        <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-	                            <path d="M22 12a10 10 0 1 0-11.6 9.9v-7H8v-3h2.4V9.8c0-2.4 1.4-3.8 3.6-3.8 1 0 2.1.2 2.1.2v2.3h-1.2c-1.2 0-1.6.8-1.6 1.5V12H16l-.4 3h-2.2v7A10 10 0 0 0 22 12z"/>
-	                        </svg>
-	                    </button>
-	                    <button type="button" class="share-icon-btn" data-action="share-instagram" aria-label="Share to Instagram" title="Instagram">
-	                        <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-	                            <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm10 2H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-5 4.5A5.5 5.5 0 1 1 6.5 14 5.5 5.5 0 0 1 12 8.5zm0 2A3.5 3.5 0 1 0 15.5 14 3.5 3.5 0 0 0 12 10.5zM18 7a1 1 0 1 1-1 1 1 1 0 0 1 1-1z"/>
-	                        </svg>
-	                    </button>
-	                    <button type="button" class="share-icon-btn" data-action="share-x" aria-label="Share on X" title="X">
-	                        <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-	                            <path d="M18.9 2H22l-6.8 7.8L23 22h-6.8l-5.3-6.5L5 22H2l7.4-8.5L1.4 2H8.3l4.8 5.8L18.9 2zm-1.2 18h1.7L7.3 3.9H5.5L17.7 20z"/>
-	                        </svg>
+	                <div class="actions-right" aria-label="Recipe actions right">
+	                    <div class="share-actions" aria-label="Share recipe">
+	                        <button type="button" class="share-icon-btn" data-action="share-facebook" aria-label="Share on Facebook" title="Facebook">
+	                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+	                                <path d="M22 12a10 10 0 1 0-11.6 9.9v-7H8v-3h2.4V9.8c0-2.4 1.4-3.8 3.6-3.8 1 0 2.1.2 2.1.2v2.3h-1.2c-1.2 0-1.6.8-1.6 1.5V12H16l-.4 3h-2.2v7A10 10 0 0 0 22 12z"/>
+	                            </svg>
+	                        </button>
+	                        <button type="button" class="share-icon-btn" data-action="share-instagram" aria-label="Share to Instagram" title="Instagram">
+	                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+	                                <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm10 2H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-5 4.5A5.5 5.5 0 1 1 6.5 14 5.5 5.5 0 0 1 12 8.5zm0 2A3.5 3.5 0 1 0 15.5 14 3.5 3.5 0 0 0 12 10.5zM18 7a1 1 0 1 1-1 1 1 1 0 0 1 1-1z"/>
+	                            </svg>
+	                        </button>
+	                        <button type="button" class="share-icon-btn" data-action="share-x" aria-label="Share on X" title="X">
+	                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+	                                <path d="M18.9 2H22l-6.8 7.8L23 22h-6.8l-5.3-6.5L5 22H2l7.4-8.5L1.4 2H8.3l4.8 5.8L18.9 2zm-1.2 18h1.7L7.3 3.9H5.5L17.7 20z"/>
+	                            </svg>
+	                        </button>
+	                    </div>
+	                    <button type="button" class="action-btn print-btn" data-action="print-recipe">
+	                        🖨️ Print Recipe 
 	                    </button>
 	                </div>
-	                <button type="button" class="action-btn print-btn" data-action="print-recipe">
-	                    🖨️ Print Recipe 
-	                </button>
 	            </div>
 	        </div>
 	    `;
