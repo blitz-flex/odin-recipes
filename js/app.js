2065a2c700ca5a5a02764d7d89d718a8ed18ea76
blitz-flex <abashidzeikaa@gmail.com>
2026-02-09 22:24:30 +0400

    Refine modal share icons and video link label

--- js/app.js
+++ js/app.js
@@ -546,10 +546,12 @@ async function showRecipe(id) {
 	                               rel="noopener noreferrer"
 	                               data-print-url="${formatPrintUrl(recipe.strYoutube)}"
 	                               class="video-link">
-	                                <svg class="video-link-icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-	                                    <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm-2.2 6.9 7 3.9-7 3.9V8.9z"/>
-	                                </svg>
-	                                <span>Watch Video Tutorial</span>
+	                                <span class="video-link-icon-wrap" aria-hidden="true">
+	                                    <svg class="video-link-icon" viewBox="0 0 24 24" focusable="false">
+	                                        <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm-2.2 6.9 7 3.9-7 3.9V8.9z"/>
+	                                    </svg>
+	                                </span>
+	                                <span>Video Tutorial</span>
 	                            </a>
 	                        </div>
 	                    ` : ''}
@@ -560,19 +562,21 @@ async function showRecipe(id) {
 		                    <div class="share-actions" aria-label="Share recipe">
 		                        <button type="button" class="share-icon-btn" data-action="share-facebook" aria-label="Share on Facebook" title="Facebook">
 		                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-		                                <path d="M22 12a10 10 0 1 0-11.6 9.9v-7H8v-3h2.4V9.8c0-2.4 1.4-3.8 3.6-3.8 1 0 2.1.2 2.1.2v2.3h-1.2c-1.2 0-1.6.8-1.6 1.5V12H16l-.4 3h-2.2v7A10 10 0 0 0 22 12z"/>
+		                                <path d="M14 9.4h2.3V6.7C16 6.7 14.9 6.5 13.6 6.5c-2.3 0-3.7 1.4-3.7 3.9v2.2H7.6v2.7h2.3V20h2.9v-4.7h2.5l.4-2.7h-2.9v-2c0-.8.2-1.2 1.2-1.2z"/>
 		                            </svg>
-	                        </button>
-	                        <button type="button" class="share-icon-btn" data-action="share-instagram" aria-label="Share to Instagram" title="Instagram">
-	                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-	                                <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm10 2H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-5 4.5A5.5 5.5 0 1 1 6.5 14 5.5 5.5 0 0 1 12 8.5zm0 2A3.5 3.5 0 1 0 15.5 14 3.5 3.5 0 0 0 12 10.5zM18 7a1 1 0 1 1-1 1 1 1 0 0 1 1-1z"/>
-	                            </svg>
-	                        </button>
-	                        <button type="button" class="share-icon-btn" data-action="share-x" aria-label="Share on X" title="X">
-	                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-	                                <path d="M18.9 2H22l-6.8 7.8L23 22h-6.8l-5.3-6.5L5 22H2l7.4-8.5L1.4 2H8.3l4.8 5.8L18.9 2zm-1.2 18h1.7L7.3 3.9H5.5L17.7 20z"/>
-	                            </svg>
-	                        </button>
+		                        </button>
+		                        <button type="button" class="share-icon-btn" data-action="share-instagram" aria-label="Share to Instagram" title="Instagram">
+		                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+		                                <path d="M7.5 3.5h9A4.5 4.5 0 0 1 21 8v8a4.5 4.5 0 0 1-4.5 4.5h-9A4.5 4.5 0 0 1 3 16V8a4.5 4.5 0 0 1 4.5-4.5zm9 2h-9A2.5 2.5 0 0 0 5 8v8a2.5 2.5 0 0 0 2.5 2.5h9A2.5 2.5 0 0 0 19 16V8a2.5 2.5 0 0 0-2.5-2.5z"/>
+		                                <path d="M12 8.5A3.5 3.5 0 1 1 8.5 12 3.5 3.5 0 0 1 12 8.5zm0 2A1.5 1.5 0 1 0 13.5 12 1.5 1.5 0 0 0 12 10.5z"/>
+		                                <path d="M16.8 7.2a1 1 0 1 1-1-1 1 1 0 0 1 1 1z"/>
+		                            </svg>
+		                        </button>
+		                        <button type="button" class="share-icon-btn" data-action="share-x" aria-label="Share on X" title="X">
+		                            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+		                                <path d="M4 4h3.4l4.1 5.6L16.7 4H20l-6.4 7.6L20.6 20h-3.4l-4.5-6.1L7.3 20H4l6.9-7.9L4 4z"/>
+		                            </svg>
+		                        </button>
 		                    </div>
 		                    <button type="button" class="action-btn print-btn" data-action="print-recipe">
 		                        🖨️ Print Recipe
